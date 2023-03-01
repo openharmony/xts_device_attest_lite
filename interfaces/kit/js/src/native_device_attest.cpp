20d5b03d68d53f7d3eae09931ddf920dc7139ae9
denghaidong <1753241333@qq.com>
2023-03-01 16:15:00 +0800

    reset code
    Signed-off-by: denghaidong <1753241333@qq.com> 834a40f

--- interfaces/kit/js/src/native_device_attest.cpp
+++ interfaces/kit/js/src/native_device_attest.cpp
@@ -66,7 +66,7 @@ void SuccessCallBack(const JSIValue thisVal, const JSIValue args, JSIValue jsiVa
     JSIValue error = JSI::CreateUndefined();
     JSIValue argv[ARGC_TWO] = {error, jsiValue};
     JSI::CallFunction(args, thisVal, argv, ARGC_TWO);
-    JSI::ReleaseValueList(error, jsiValue, ARGS_END);
+    JSI::ReleaseValueList(error, ARGS_END);
 }
 
 bool IsValidJSIValue(const JSIValue* args, uint8_t argsNum)
@@ -158,7 +158,7 @@ void ExecuteGetAttestResult(void* data)
     } else {
         SuccessCallBack(thisVal, args, result);
     }
-    JSI::ReleaseValueList(args, thisVal, result, ARGS_END);
+    JSI::ReleaseValueList(args, thisVal, ARGS_END);
     delete params;
     params = nullptr;
     return;
