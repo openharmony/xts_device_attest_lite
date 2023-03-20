0a12630ae8b3620a5b65fff63a101699e2229c5a
keminLuo <luojianming4@h-partners.com>
2023-03-20 14:01:57 +0800

    消除ci看板告警
    Signed-off-by: 罗键铭 <541416002@qq.com>

--- framework/mini/src/attest_framework_client_mini.c
+++ framework/mini/src/attest_framework_client_mini.c
@@ -34,10 +34,3 @@ int32_t GetAttestStatus(AttestResultInfo* attestResultInfo)
 {
     return EntryGetAttestStatus(attestResultInfo);
 }
-
-void ThreadMain(void)
-{
-    // 开机自启动
-}
-
-APP_FEATURE_INIT(ThreadMain);
